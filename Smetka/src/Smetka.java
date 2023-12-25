7c58ae5dacdabd424e725e4f66b26746bb0990d8
pitomacAhmed <148368787+pitomacAhmed@users.noreply.github.com>
2023-12-25 17:48:25 +0100

    Add files via upload

---
+++ Smetka/src/Smetka.java
@@ -0,0 +1,14 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+	int J = 32;
+	int K = 20;
+	int G = 50;
+	double R = (J * 2) + (K * 5) + (G * 2.5);
+
+	System.out.println ("ukupan racun je:");
+	System.out.print (R);
+	}
+
+}
